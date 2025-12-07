55f3d4d4687ac7c89bd003b2464a485e59723571
Muhammad Fajar Sholehuddin Maulana Putra <152793334+sholehuddinn@users.noreply.github.com>
2025-12-07 22:56:14 +0700

    dr

--- src/services/tutorialService.js
+++ src/services/tutorialService.js
@@ -54,16 +54,10 @@ export const fetchTutorials = async () => {
 
 export const fetchTutorialId = async (id) => {
   try {
-    const cachedTutorial = await redisClient.get(`tutorial:${id}`);
-
-    if (cachedTutorial) {
-      return JSON.parse(cachedTutorial);
-    }
 
     const response = await fetch(`${process.env.API_DICODING}/tutorials/${id}`);
     const tutorial = await response.json();
 
-    await redisClient.set(`tutorial:${id}`, JSON.stringify(tutorial), { EX: 3600 });
     return tutorial;
   } catch (error) {
     throw error;
