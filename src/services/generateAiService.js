4e6322ac44e04fbd9ee2fea3819cb2dfab85e513
Muhammad Fajar Sholehuddin Maulana Putra <152793334+sholehuddinn@users.noreply.github.com>
2025-12-12 09:05:57 +0700

    Update generateAiService.js

--- src/services/generateAiService.js
+++ src/services/generateAiService.js
@@ -7,7 +7,7 @@ const ai = new GoogleGenAI({
 export async function generateAI(prompt) {
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-1.5-flash",
       contents: prompt
     });
 
