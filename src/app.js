ecb1c8f4fec9144a04f017d4152470f79ed4db9c
Muhammad Fajar Sholehuddin Maulana Putra <152793334+sholehuddinn@users.noreply.github.com>
2026-01-17 22:33:21 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,7 @@ import cors from 'cors';
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: ["https://capstone.web.id"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-app.use(express.json());
-
-app.options("*", cors());
+app.use(cors());
 
 app.use(express.json());
 app.use('/', routes);
